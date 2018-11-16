34e84526cc199f67d01dc10576981b7c46614e10
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-11-16 16:25:46 -0600

    rf package edited.

--- src/photonics/rf/MicrostripLine.java
+++ src/photonics/rf/MicrostripLine.java
@@ -1,5 +1,70 @@
 package photonics.rf;
 
-public class MicrostripLine {
+import ch.epfl.general_libraries.clazzes.ParamName;
+import static java.lang.Math.*;
 
+import java.util.HashMap;
+import java.util.Map; 
+
+public class MicrostripLine {
+	
+	double w, t, h, epsr ;
+	
+	public MicrostripLine(
+			@ParamName(name="Width of metal (m)") double w,
+			@ParamName(name="Thickness of metal (m)") double t,
+			@ParamName(name="Thickness of substrate (m)") double h,
+			@ParamName(name="Relative permittivity of substrate") double epsr
+			) {
+		this.w = w ;
+		this.t = t ;
+		this.h = h ;
+		this.epsr = epsr ;
+	}
+	
+	public Map<String, String> getAllParameters() {
+		Map<String, String> map = new HashMap<>() ;
+		map.put("w", w+"") ;
+		map.put("h", h+"") ;
+		map.put("t", t+"") ;
+		map.put("epsr", epsr+"") ;
+		return map ;
+	}
+
+	public double getImpedance() {
+		double epsEff = getEpsEff() ;
+		double q0 = pow(t/h, 2.0) + pow((1.0/PI)/(w/t+1.1), 2.0) ;
+		double dW = t/PI*log(4*E/q0) ;
+		double q1 = (1.0+1.0/epsEff)/2.0 ;
+		double dWprime = dW*q1 ;
+		double wprime = w + dWprime ;
+		double a0 = 120.0*PI/2.0/sqrt(2.0)/PI/sqrt(epsr+1.0) ;
+		double a1 = 4.0*h/wprime ;
+		double a2 = (14+8.0/epsEff)/11.0 * 4*h/wprime ;
+		double a3 = sqrt(pow(a2, 2.0) + PI*PI*q1) ;
+		double Z0 = a0 * log(1.0 + a1 * (a2+a3)) ;
+		return Z0 ;
+	}
+	
+	public double getEpsEff() {
+		if(w/h < 1) {
+			double a0 = (epsr+1)/2.0 ;
+			double a1 = (epsr-1)/2.0 ;
+			double a2 = 1.0/sqrt(1+12.0*h/w) ;
+			double a3 = 0.04 * pow(1.0-w/h, 2.0) ;
+			return a0 + a1 * (a2 + a3) ;
+		}
+		else {
+			double a0 = (epsr+1)/2.0 ;
+			double a1 = (epsr-1)/2.0 ;
+			double a2 = 1.0/sqrt(1+12.0*h/w) ;
+			return a0 + a1 * a2 ;
+		}
+	}
+	
+	public double getNeff() {
+		return sqrt(getEpsEff()) ;
+	}
+	
+	
 }
---
+++ src/photonics/rf/TestMicrostrip.java
@@ -0,0 +1,40 @@
+package photonics.rf;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.StandardChartTheme;
+
+import ch.epfl.general_libraries.experiment_aut.Experiment;
+import ch.epfl.general_libraries.experiment_aut.WrongExperimentException;
+import ch.epfl.general_libraries.results.AbstractResultsDisplayer;
+import ch.epfl.general_libraries.results.AbstractResultsManager;
+import ch.epfl.general_libraries.results.DataPoint;
+import ch.epfl.javancox.experiments.builder.ExperimentConfigurationCockpit;
+
+public class TestMicrostrip implements Experiment {
+
+	MicrostripLine line ;
+	
+	public TestMicrostrip(
+			MicrostripLine line
+			) {
+		this.line = line ;
+	}
+	
+	@Override
+	public void run(AbstractResultsManager man, AbstractResultsDisplayer dis) throws WrongExperimentException {
+		DataPoint dp = new DataPoint() ;
+		dp.addProperties(line.getAllParameters());
+		dp.addResultProperty("impedance (ohm)", line.getImpedance());
+		dp.addResultProperty("eps_eff", line.getEpsEff());
+		dp.addResultProperty("neff", line.getNeff());
+		man.addDataPoint(dp);
+	}
+	
+	public static void main(String[] args) {
+		ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
+		String packageName = "photonics" ;
+		String className = TestMicrostrip.class.getName() ;
+		ExperimentConfigurationCockpit.execute(new String[] {"-p", packageName, "-c", className}, true);
+	}
+
+}
