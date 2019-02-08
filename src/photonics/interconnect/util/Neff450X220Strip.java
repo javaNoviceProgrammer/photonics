39671117c410031f6d735aacbdc51c161e8c5078
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-02-08 09:55:07 -0600

    minor changes to interconnect.

---
+++ src/photonics/interconnect/util/Neff450X220Strip.java
@@ -0,0 +1,13 @@
+package photonics.interconnect.util;
+
+import mathLib.func.intf.RealFunction;
+
+public class Neff450X220Strip implements RealFunction {
+
+	@Override
+	public double evaluate(double lambdaNm) {
+		// ToDo complete this
+		return 2.23 ;
+	}
+
+}
