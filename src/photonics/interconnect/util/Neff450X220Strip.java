2944c9743b63cdd9eb5e444a989c4364b8e94356
javaNoviceProgrammer <meisam@c-73-45-141-193.hsd1.il.comcast.net>
2019-02-08 20:00:50 -0600

    minor change.

--- src/photonics/interconnect/util/Neff450X220Strip.java
+++ src/photonics/interconnect/util/Neff450X220Strip.java
@@ -4,10 +4,13 @@ import mathLib.func.intf.RealFunction;
 
 public class Neff450X220Strip implements RealFunction {
 
+	double A2 = -2.6821e-7,
+		   A1 = -0.00048639,
+		   A0 = 3.7488 ;
+
 	@Override
 	public double evaluate(double lambdaNm) {
-		// ToDo complete this
-		return 2.23 ;
+		return A2*lambdaNm*lambdaNm + A1*lambdaNm + A0 ;
 	}
 
 }
