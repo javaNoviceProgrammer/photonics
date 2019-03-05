19374d85600ea731605b6b61674f137a99411083
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-03-05 15:18:37 -0600

    more changes to interconnect.

---
+++ src/photonics/interconnect/elements/passive/Termination.java
@@ -0,0 +1,50 @@
+package photonics.interconnect.elements.passive;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import ch.epfl.general_libraries.utils.SimpleMap;
+import mathLib.numbers.Complex;
+import mathLib.sfg.numeric.SFG;
+import photonics.interconnect.elements.AbstractElement;
+import photonics.util.Wavelength;
+
+public class Termination extends AbstractElement {
+	
+	Wavelength inputLambda = null ;
+	
+	Complex s11 = Complex.ZERO ;
+	
+	public void setName(String name) {
+		this.name = name ;
+	}
+	
+	@Override
+	public void setWavelength(Wavelength inputLambda) {
+		this.inputLambda = inputLambda ;
+	}
+
+	@Override
+	public void buildElement() {
+		
+		if(inputLambda == null)
+			throw new NullPointerException("wavelength is not set for " + name) ;
+		
+		String port1_in = name+".port1.in" ;
+		String port1_out = name+".port1.out" ;
+
+		nodes = new ArrayList<>() ;
+		nodes.add(port1_in) ;
+		nodes.add(port1_out) ;
+
+		sfgElement = new SFG(nodes) ;
+		sfgElement.addArrow(port1_in, port1_out, s11);
+	}
+
+	@Override
+	public Map<String, String> getAllParameters() {
+		Map<String, String> map = new SimpleMap<String, String>() ;
+		return map ;
+	}
+
+}
--- src/photonics/interconnect/tutorial/Tutorial15.java
+++ src/photonics/interconnect/tutorial/Tutorial15.java
@@ -13,42 +13,40 @@ import photonics.util.Wavelength;
 public class Tutorial15 {
 	
 	public static void main(String[] args) {
-		double L = 36.8 ; // um
-		double Lwg = 40 ; // um
+//		double L = 36.8 ; // um
+		double Ldc1 = 34 ; // um
+		double Ldc2 = 34 ; // um
+		double L3 = 20 ; // um
+		double Lwg = 30 ; // um
 		double gap = 200e-3 ; // um
 		double loss = 0 ; // dB per cm
-		double[] lambdaNm = MathUtils.linspace(1530, 1570, 1000) ;
-		double[] r1 = new double[lambdaNm.length] ; 
-		double[] r2 = new double[lambdaNm.length] ; 
-		double[] r3 = new double[lambdaNm.length] ; 
-		double[] r4 = new double[lambdaNm.length] ; 
-		double[] r10 = new double[lambdaNm.length] ; 
+		double[] lambdaNm = MathUtils.linspace(1545, 1555, 10000) ;
+		double[] transfer = new double[lambdaNm.length] ; 
 		for(int i=0; i<lambdaNm.length; i++) {
 			Wavelength lambda = new Wavelength(lambdaNm[i]) ;
 			PhotonicCircuit pc = new PhotonicCircuit() ;
 			pc.setWavelength(lambda) ;
-			DirectionalCoupler dc1 = new DirectionalCoupler("dc1", new Neff450X220CoupledStrip(), L, gap, loss) ;
+			DirectionalCoupler dc1 = new DirectionalCoupler("dc1", new Neff450X220CoupledStrip(), Ldc1, gap, loss) ;
+			DirectionalCoupler dc2 = new DirectionalCoupler("dc2", new Neff450X220CoupledStrip(), Ldc2, gap, loss) ;
 			StraightWg wg1 = new StraightWg("wg1", new Neff450X220Strip(), loss, Lwg) ;
-			pc.addElements(dc1);
-			pc.addElement(wg1);
-			pc.connectPorts("dc1.port2", "wg1.port1");
-			pc.connectPorts("wg1.port2", "dc1.port3");
-			r1[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc1.port1").absSquared()) ;
-			r2[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc1.port1").pow(2).absSquared()) ;
-			r3[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc1.port1").pow(3).absSquared()) ;
-			r4[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc1.port1").pow(4).absSquared()) ;
-			r10[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc1.port1").pow(10).absSquared()) ;
+			StraightWg wg2 = new StraightWg("wg2", new Neff450X220Strip(), loss, Lwg) ;
+			StraightWg wg3 = new StraightWg("wg3", new Neff450X220Strip(), loss, L3) ;
+			pc.addElements(dc1, dc2);
+			pc.addElements(wg1, wg2, wg3);
+			pc.connectPorts("dc1.port2", "wg1.port2");
+			pc.connectPorts("wg1.port1", "dc1.port3");
+			pc.connectPorts("dc2.port2", "wg2.port2");
+			pc.connectPorts("wg2.port1", "dc2.port3");
+			pc.connectPorts("dc1.port4", "wg3.port1");
+			pc.connectPorts("dc2.port1", "wg3.port2");
+			transfer[i] = Utils.todB(pc.getTransfer("dc1.port1", "dc2.port4").absSquared()) ;
 		}
 
 		MatlabChart fig = new MatlabChart() ;
-		fig.plot(lambdaNm, r1, "b", 2f, "r1");
-		fig.plot(lambdaNm, r2, "r", 2f, "r2");
-		fig.plot(lambdaNm, r3, "m", 2f, "r3");
-		fig.plot(lambdaNm, r4, "k", 2f, "r4");
-		fig.plot(lambdaNm, r10, "g", 2f, "r10");
+		fig.plot(lambdaNm, transfer);
 		fig.renderPlot();
 		fig.xlabel("Wavelength (nm)");
-		fig.ylabel("Reflection (dB)");
+		fig.ylabel("Transmission (dB)");
 		fig.run(true);
 	}
 }
