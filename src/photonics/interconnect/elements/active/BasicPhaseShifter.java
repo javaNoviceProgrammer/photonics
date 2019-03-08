d025735baf94aad6c17056bda22806fbb41cfea7
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-03-08 14:48:00 -0600

    minor change.

--- src/photonics/interconnect/elements/active/BasicPhaseShifter.java
+++ src/photonics/interconnect/elements/active/BasicPhaseShifter.java
@@ -1,5 +1,44 @@
 package photonics.interconnect.elements.active;
 
-public class BasicPhaseShifter {
+import java.util.HashMap;
+import java.util.Map;
+
+import ch.epfl.general_libraries.clazzes.ParamName;
+import mathLib.numbers.Complex;
+import photonics.interconnect.elements.AbstractElement;
+import photonics.util.Wavelength;
+
+public class BasicPhaseShifter extends AbstractElement {
+	
+	double deltaPhi, excessLossdB ;
+	Wavelength lambda ;
+	Complex s11, s12, s21, s22 ;
+	
+	public BasicPhaseShifter(
+			@ParamName(name="Phase shift (rad)") double deltaPhi,
+			@ParamName(name="Excess Loss (dB)") double excessLossdB
+			) {
+		this.deltaPhi = deltaPhi ;
+		this.excessLossdB = excessLossdB ;
+	}
+
+	@Override
+	public void buildElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setWavelength(Wavelength inputLambda) {
+		this.lambda = inputLambda ;
+	}
+
+	@Override
+	public Map<String, String> getAllParameters() {
+		Map<String, String> map = new HashMap<>() ;
+		map.put("delta phi (rad)", deltaPhi+"") ;
+		map.put("excess loss (dB)", excessLossdB+"") ;
+		return map;
+	}
 
 }
