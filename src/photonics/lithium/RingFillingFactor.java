27963efafb9ed31fb8e1d6c6dbf8fbe3bff030a5
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-05-08 08:24:24 -0500

    minor changes.

--- src/photonics/lithium/RingFillingFactor.java
+++ src/photonics/lithium/RingFillingFactor.java
@@ -2,6 +2,7 @@ package photonics.lithium;
 
 import static photonics.lithium.EOCoeff.*;
 import static photonics.lithium.EpsilonLN.*;
+import static photonics.lithium.Crystal.*;
 
 import mathLib.func.ArrayFunc;
 import mathLib.func.intf.RealFunction2D;
@@ -21,6 +22,7 @@ public class RingFillingFactor {
 		switch (cut) {
 			case Xcut:
 				factor = cos(phi)*cos(phi)*cos(phi) + (eps22*eps22)/(eps33*eps33)*(r22/r33 * sin(phi) + r23/r33*cos(phi))*sin(phi)*sin(phi) ;
+//				factor = cos(phi)*cos(phi)*cos(phi) ;
 				break;
 			case Ycut:
 				factor = cos(phi)*cos(phi)*cos(phi) + (eps11*eps11)/(eps33*eps33)*(r13/r33*cos(phi))*sin(phi)*sin(phi) ;
@@ -40,17 +42,17 @@ public class RingFillingFactor {
 		return 1.0/(2*PI)*integral.getIntegral() ;
 	}
 	
-	private void plotFF(Crystal cut) {
+	public void plotFF(Crystal cut) {
 		double[] phi = MathUtils.linspace(0, PI, 500) ;
 		double[] ff = ArrayFunc.apply(t -> getFF(cut, -t, t), phi) ;
 		MatlabChart fig = new MatlabChart() ;
-		fig.plot(ArrayFunc.apply(x -> 180.0/PI*x, phi), ff);
+		fig.plot(ArrayFunc.apply(x -> 180.0/PI*x, phi), ff, "b", 1f, cut.name());
 		fig.renderPlot();
+		fig.legendON();
 		fig.run(true);
 	}
 	
-	
-	private void plotContours(Crystal cut) {
+	public void plotContours(Crystal cut) {
 		double[] phi0 = MathUtils.linspace(-PI, 0, 500) ;
 		double[] phi1 = MathUtils.linspace(0, PI, 500) ;
 		MeshGrid meshGrid = new MeshGrid(phi0, phi1) ;
@@ -61,9 +63,23 @@ public class RingFillingFactor {
 	
 	public static void main(String[] args) {
 		RingFillingFactor rff = new RingFillingFactor() ;
-//		rff.plotContours(Crystal.Xcut);
-		rff.plotFF(Crystal.Xcut);
-//		rff.plotFF(Crystal.Ycut);
+//		rff.plotContours(Xcut);
+//		rff.plotFF(Xcut);
+//		rff.plotFF(Ycut);
+		
+//		double[] x = MathUtils.linspace(0, 2*PI, 100) ;
+//		double[] y = ArrayFunc.apply(t -> rff.getFF(Ycut, 0, t), x) ;
+//		MatlabChart fig = new MatlabChart() ;
+//		fig.plot(ArrayFunc.apply(t -> 180.0/PI*t, x), y);
+//		fig.renderPlot();
+//		fig.run(true);
+		
+		double[] x = MathUtils.linspace(0, PI, 100) ;
+		double[] y = ArrayFunc.apply(t -> rff.getFF(Ycut, PI/2.0-t, PI/2.0+t), x) ;
+		MatlabChart fig = new MatlabChart() ;
+		fig.plot(ArrayFunc.apply(t -> 180.0/PI*t, x), y);
+		fig.renderPlot();
+		fig.run(true);
 	}
 
 }
