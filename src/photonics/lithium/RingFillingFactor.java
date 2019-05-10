83d1ae3d014cc4a01a54e8d202c4a1bb6a55b268
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2019-05-10 07:39:25 -0500

    minor change.

--- src/photonics/lithium/RingFillingFactor.java
+++ src/photonics/lithium/RingFillingFactor.java
@@ -81,9 +81,9 @@ public class RingFillingFactor {
 //		fig.renderPlot();
 //		fig.run(true);
 		
-		double[] x = MathUtils.linspace(-PI/2.0, PI/2.0, 100) ;
-//		double[] y = ArrayFunc.apply(t -> rff.getDneff(Xcut, t), x) ;
-		double[] y = ArrayFunc.apply(t -> rff.getFF(Xcut, -PI/2.0+t, PI/2.0+t)*2, x) ;
+		double[] x = MathUtils.linspace(-PI, PI, 100) ;
+		double[] y = ArrayFunc.apply(t -> rff.getDneff(Ycut, t), x) ;
+//		double[] y = ArrayFunc.apply(t -> rff.getFF(Xcut, -PI/2.0+t, PI/2.0+t)*2, x) ;
 //		double[] y = ArrayFunc.apply(t -> rff.getFF(Xcut, -PI/2.0-t, PI/2.0-t) - rff.getFF(Xcut, PI/2.0-t, 3*PI/2.0-t), x) ;
 		MatlabChart fig = new MatlabChart() ;
 		fig.plot(ArrayFunc.apply(t -> 180.0/PI*t, x), y);
